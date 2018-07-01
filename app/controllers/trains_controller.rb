e5dbf307b56754e2cbef55bdca42f62e3f8fc729
itrypolskyi <ihor.trypolskyi@gmail.com>
2018-07-01 12:20:18 +0300

    Completed lesson 13

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -64,6 +64,6 @@ class TrainsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def train_params
-      params.require(:train).permit(:number)
+      params.require(:train).permit(:number, :route_id)
     end
 end
